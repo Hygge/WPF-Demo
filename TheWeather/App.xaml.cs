d21d92507a1e54eecb075eebfe778f6076673e27
hygge <3163367790@qq.com>
2025-12-18 17:18:00 +0800

    忘记提交

---
+++ TheWeather/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Application = System.Windows.Application;
+
+namespace TheWeather;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private NotifyIcon _notifyIcon;
+    public static MainWindow _mainWindow = new MainWindow();
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _mainWindow.ShowInTaskbar = false;
+        CreateNotifyIcon();
+        base.OnStartup(e);
+        _mainWindow.Show();
+    }
+
+    private void CreateNotifyIcon()
+    {
+        _notifyIcon = new NotifyIcon
+        {
+            Icon = new System.Drawing.Icon("app_icon.ico"), // 添加 .ico 文件到项目
+            Visible = true,
+            Text = "天气小工具"
+        };
+
+        // 双击显示主窗口（可选）
+        _notifyIcon.DoubleClick += (s, args) =>
+        {
+            _mainWindow.Show();
+        };
+
+        // 右键菜单
+        var contextMenu = new ContextMenuStrip();
+        contextMenu.Items.Add("显示", null, (s, args) => _mainWindow.Show());
+        contextMenu.Items.Add("隐藏", null, (s, args) => _mainWindow.Hide());
+        contextMenu.Items.Add("退出", null, (s, args) => Shutdown());
+        _notifyIcon.ContextMenuStrip = contextMenu;
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _notifyIcon?.Dispose(); // 释放资源
+        base.OnExit(e);
+    }
+}
---
+++ TheWeather/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using TheWeather.ViewModels;
+using Application = System.Windows.Application;
+
+namespace TheWeather;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.DataContext = new MainWindowViewModel();
+    }
+
+    private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if(e.LeftButton ==  MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+    private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+    private void MenuItem_OnClick_Hide(object sender, RoutedEventArgs e)
+    {
+        App._mainWindow.Hide();
+    }
+}
